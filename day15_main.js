ae5b70f2662e5abe0cc74c3048e658cc1bfeaee1
bojana-petroska <bojana.petroska@yahoo.com>
2024-05-30 12:34:39 +0200

    2

--- day15_main.js
+++ day15_main.js
@@ -2,21 +2,30 @@ const number1 = document.getElementById('numInput');
 const number2 = document.getElementById('num2Input');
 
 const buttonElement = document.getElementById('addNumButton');
-const displayResult = document.getElementById('display-result')
+const displayResult = document.getElementById('display-result');
 const clearButton = document.getElementById('clear-button');
-const displayRandom = document.getElementById('display-random-number')
 
-const buttonGenerateRandomNumber = document.getElementById('generate-random-number')
-const buttonClearRandomNumber = document.getElementById('clear-random-number')
+const buttonGenerateRandomNumber = document.getElementById('generate-random-number');
+const displayRandom = document.getElementById('display-random-number');
+const buttonClearRandomNumber = document.getElementById('clear-random-number');
 
 function addNumbers() {
-    const inputNum = number1.value;
-    const inputNum2 = number2.value;
-
-    result = parseInt(inputNum) + parseInt(inputNum2);
+    let result = parseInt(number1.value) + parseInt(number2.value);
+    console.log(result);
 
     displayResult.innerText = result;
 };
+
+// function addNumbers() {
+//     const display = document.createElement('div');
+//     display.className = 'display';
+//     let displayText = document.createElement('div');
+
+//     displayText.innerText = parseInt(number1.value) + parseInt(number2.value);
+//     console.log(displayText);
+
+//     display.appendChild(displayText);
+// };
 
 buttonElement.addEventListener('click', function() {
     addNumbers();
@@ -38,5 +47,3 @@ buttonGenerateRandomNumber.addEventListener('click', function() {
 buttonClearRandomNumber.addEventListener('click', function() {
     displayRandom.innerText = "display random number here"
 });
-
-
